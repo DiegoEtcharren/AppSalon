5dc2a9d6caf1435882ae31234ebcda258acdb2bd
Diego Etcharren <diegoe96@gmail.com>
2025-02-12 14:57:37 -0600

    JS

--- public/build/js/app.js
+++ public/build/js/app.js
@@ -1,1 +1,28 @@
-alert("Desde app JS");
+let paso = 1;
+
+document.addEventListener('DOMContentLoaded', function(){
+    iniciarApp();
+});
+
+function iniciarApp() {
+    tabs(); // Cambiar seccion cuando se presionen los tabs. 
+    mostrarSeccion(); // 
+
+}
+
+function mostrarSeccion() {
+    // Seleccionar la seccion con el paso:
+    const pasoSelector = `#paso-${paso}`;
+    const seccion = document.querySelector(pasoSelector);
+}
+
+function tabs() {
+    const botones = document.querySelectorAll('.tabs button');
+    botones.forEach(boton => {
+        boton.addEventListener('click', function(e) {
+            paso = parseInt(e.target.dataset.paso);
+            mostrarSeccion();
+        });
+    }); 
+}
+
