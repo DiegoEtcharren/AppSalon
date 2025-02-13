433aa938e711dd9576bca63a26b046f4ca90b861
Diego Etcharren <diegoe96@gmail.com>
2025-02-12 18:05:10 -0600

    JS

--- public/build/js/app.js
+++ public/build/js/app.js
@@ -7,13 +7,33 @@ document.addEventListener('DOMContentLoaded', function(){
 function iniciarApp() {
     tabs(); // Cambiar seccion cuando se presionen los tabs. 
     mostrarSeccion(); // 
-
+    botonesPaginador(); 
+    paginaSiguiente();
+    paginaAnterior();
 }
 
 function mostrarSeccion() {
+
+    // Ocultar la seccion de clase que tenga mostar: 
+    const seccionAnterior = document.querySelector('.mostrar');
+    if (seccionAnterior) {
+        seccionAnterior.classList.remove('mostrar');
+    }
+
     // Seleccionar la seccion con el paso:
     const pasoSelector = `#paso-${paso}`;
     const seccion = document.querySelector(pasoSelector);
+    seccion.classList.toggle('mostrar');
+
+    // Cambia estado del tab previo: 
+    const tabAnterior = document.querySelector('.actual');
+    if (tabAnterior) {
+        tabAnterior.classList.remove('actual');
+    }
+    
+    // Resalta el tab actual:
+    const tab = document.querySelector(`[data-paso="${paso}"]`);
+    tab.classList.toggle('actual');
 }
 
 function tabs() {
@@ -22,7 +42,40 @@ function tabs() {
         boton.addEventListener('click', function(e) {
             paso = parseInt(e.target.dataset.paso);
             mostrarSeccion();
+            botonesPaginador();
         });
     }); 
 }
+
+function botonesPaginador() {
+    const paginaAnterior = document.querySelector("#anterior");
+    const paginaSiguiente = document.querySelector("#siguiente");
+    
+    if (paso === 1) {
+        paginaAnterior.classList.add('ocultar');
+        paginaSiguiente.classList.remove('ocultar');
+    } else if (paso === 3) {
+        paginaAnterior.classList.remove('ocultar');
+        paginaSiguiente.classList.add('ocultar');
+    } else {
+        paginaAnterior.classList.remove('ocultar');
+        paginaSiguiente.classList.remove('ocultar');
+    }
+}
+
+function paginaAnterior() {
+    const paginaAnterior = document.querySelector("#anterior");
+    paginaAnterior.addEventListener('click', function(e) {
+        paso = paso - 1;
+    })
+}
+
+function paginaSiguiente() {
+    const paginaSiguiente = document.querySelector("#siguiente");
+    paginaSiguiente.addEventListener('click', function(e) {
+        paso = paso + 1;
+    })
+}
+
+
 
--- views/cita/index.php
+++ views/cita/index.php
@@ -3,7 +3,7 @@
 
 <div id="app">
     <nav class="tabs">
-        <button class="actual" type="button" data-paso="1">Servicios</button>
+        <button type="button" data-paso="1">Servicios</button>
         <button type="button" data-paso="2">Informacion Cita</button>
         <button type="button" data-paso="3">Resumen</button>
     </nav>
